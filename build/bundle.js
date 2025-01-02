dd6769781644d2a1b5940be0ad6595d00a3c88d7
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-02 15:40:18 -0500

    serialize initial state

--- build/bundle.js
+++ build/bundle.js
@@ -158,6 +158,10 @@ var _reactRedux = __webpack_require__(11);
 
 var _reactRouterConfig = __webpack_require__(18);
 
+var _serializeJavascript = __webpack_require__(21);
+
+var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
+
 var _Routes = __webpack_require__(7);
 
 var _Routes2 = _interopRequireDefault(_Routes);
@@ -181,7 +185,7 @@ exports.default = function (req, store) {
     )
   ));
 
-  return '\n        <html>\n            <head></head>\n            <body>\n                <div id="root">' + content + '</div>\n                <script>\n                    window.INITIAL_STATE = ' + JSON.stringify(store.getState()) + ' \n                </script>\n                <script src="bundle.js"></script>\n            </body>\n        </html>\n    ';
+  return '\n        <html>\n            <head></head>\n            <body>\n                <div id="root">' + content + '</div>\n                <script>\n                    window.INITIAL_STATE = ' + (0, _serializeJavascript2.default)(store.getState()) + ' \n                </script>\n                <script src="bundle.js"></script>\n            </body>\n        </html>\n    ';
 };
 
 /***/ }),
@@ -518,6 +522,12 @@ exports.default = {
 	component: (0, _reactRedux.connect)(mapStateToProps, { fetchUsers: _actions.fetchUsers })(UsersListPage),
 	loadData: loadData
 };
+
+/***/ }),
+/* 21 */
+/***/ (function(module, exports) {
+
+module.exports = require("serialize-javascript");
 
 /***/ })
 /******/ ]);
