2a706f168c3f28c5e7b8019aa1eb34fd8f8bf868
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-02 11:59:52 -0500

    path fix

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
-const baseConfig = require('./webpack.base/js');
+const baseConfig = require('./webpack.base.js');
 
 const config = {
 	// inform webpack we're building a bundle for nodeJS, rather than the browser
